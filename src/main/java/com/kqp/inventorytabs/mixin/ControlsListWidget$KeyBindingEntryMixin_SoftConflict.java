d49aa0ba3422f8d5c62a22c0cfc8575d5cda781e
Athar <athar@funstargate.com>
2023-06-29 15:06:19 +0200

    Change

--- src/main/java/com/kqp/inventorytabs/mixin/ControlsListWidget$KeyBindingEntryMixin_SoftConflict.java
+++ src/main/java/com/kqp/inventorytabs/mixin/ControlsListWidget$KeyBindingEntryMixin_SoftConflict.java
@@ -1,8 +1,8 @@
 package com.kqp.inventorytabs.mixin;
 
+import java.util.Objects;
+
 import com.kqp.inventorytabs.init.InventoryTabsClient;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.MutableText;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,8 +11,9 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.minecraft.client.gui.screen.option.ControlsListWidget;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * The 'Tab' keybinding conflicts with the multiplayer player list keybind, but since you can only see the player list when outside the inventory
@@ -22,19 +23,21 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class ControlsListWidget$KeyBindingEntryMixin_SoftConflict {
 	@Shadow @Final private KeyBinding binding;
 	
-	@ModifyArg(method = "render", index = 5, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawableHelper;fill(Lnet/minecraft/client/util/math/MatrixStack;IIIII)V"))
-	public int fill(MatrixStack matrices, int x1, int y1, int x2, int y2, int color) {
-		if(this.binding == InventoryTabsClient.NEXT_TAB_KEY_BIND) {
-			return Formatting.GOLD.getColorValue() | 0xFF000000;
+	@ModifyArg(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/MutableText;formatted(Lnet/minecraft/util/Formatting;)Lnet/minecraft/text/MutableText;"))
+	public Formatting setBracketsColor(Formatting color) {
+		if (this.binding == InventoryTabsClient.NEXT_TAB_KEY_BIND && color.equals(Formatting.RED)) {
+			return Formatting.GOLD;
 		}
+
 		return color;
 	}
 
-	@Redirect(method="update", at=@At(value="INVOKE", target="Lnet/minecraft/text/MutableText;formatted(Lnet/minecraft/util/Formatting;)Lnet/minecraft/text/MutableText;"))
-	public MutableText formatText(MutableText instance, Formatting formatting) {
-		if(formatting == Formatting.RED && this.binding == InventoryTabsClient.NEXT_TAB_KEY_BIND) {
-			return instance.formatted(Formatting.GOLD);
+	@ModifyArg(index = 4, method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V"))
+	public int setIndicatorColor(int color) {
+		if (this.binding == InventoryTabsClient.NEXT_TAB_KEY_BIND) {
+			return Formatting.GOLD.getColorValue() | 0xFF000000;
 		}
-		return instance.formatted(formatting);
+
+		return color;
 	}
 }
